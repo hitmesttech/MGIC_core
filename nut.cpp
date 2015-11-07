d87208c7b50a0b0eb6d87c4dedb3735cf6c74ae0
hitmesttech <hitmesttech@outlook.com>
2015-11-07 19:47:58 +0800

    Update nut.cpp

--- nut.cpp
+++ nut.cpp
@@ -1,6 +1,12 @@
-#include"stdio.h"
-#include<pthread.h>
+#define MTASK_FINISHED  2 
 
+#define MPROG_RUN       1
+#define MPROG_KILL      2
+#define MPROG_PACK      3    
+#define MPROG_FINISH    4
+
+
+int serial;
 pid_t pid;
 int flag;
 char dir[1024];
@@ -12,27 +18,34 @@ int check()
 
 int watchdog()
 {
+    FILE *ou;
+    char finish_file_dir[2048];
 	while(1)
 	{
 		if(kill(pid,0))
 		{
 			flag=0;
+            sprintf("//tmp//nutd/%ld.finish",serial);
+            ou=fopen("//tmp//nutd//","w");
+            fprintf(ou,"%ld",MTASK_FINISHED);
+            fclose(ou);
 			exit(check());
 		}
 }
 
-int gnenera== ============= ========                                                                    ===
+//int gnenera== ============= ========                                                                    ===
 
 
 int main()
 {
-	int i,j,type,cores,n;
+	int i,j,type,cores;
 	//pid_t pid;
 	pthread_t pt1;
-	char serial[1024],sdir[1024],cm[1024];
+	char sdir[1024],cm[1024];
 	char cmd[2048];
 	FILE *IN=fopen("//etc//mgicnut.conf","r");
-	scanf("%ld%ld%s",&type,&cores,serial);
+	scanf("%ld%ld%d",&type,&cores,&serial);
+	scanf("%ld%ld%d",&type,&cores,&serial);
 	fgets(sdir,1024,IN);
 	fscanf(IN,"%ld",&n);
 	if(type>n)
